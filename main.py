fe3fab98c55bc7b2c83747f97bbc16711c3ee64f
alliedmastercomputer-1 <gptvasu05042006@gmail.com>
2025-10-23 22:50:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,52 @@ with open("./data-result.json","r") as f:
 
 def convertFromFormat1 (jsonObject):
 
-    # IMPLEMENT: Conversion From Type 1
+    locationParts = jsonObject['location'].split('/')
 
-    return NotImplemented
+    result = {
+        'deviceID': jsonObject['deviceID'],
+        'deviceType': jsonObject['deviceType'],
+        'timestamp': jsonObject['timestamp'],
+        'location': {
+            'country': locationParts[0],
+            'city': locationParts[1],
+            'area': locationParts[2],
+            'factory': locationParts[3],
+            'section': locationParts[4]
+        },
+        'data': {
+            'status': jsonObject['operationStatus'],
+            'temperature': jsonObject['temp']
+        }
+    }
 
+    return result
 
 def convertFromFormat2 (jsonObject):
 
-    # IMPLEMENT: Conversion From Type 1
+    date = datetime.datetime.strptime(
+        jsonObject['timestamp'],
+        '%Y-%m-%dT%H:%M:%S.%fZ'
+    )
+    timestamp = round(
+        (date - datetime.datetime(1970, 1, 1)).total_seconds() * 1000
+    )
 
-    return NotImplemented
+    result = {
+        'deviceID': jsonObject['device']['id'],
+        'deviceType': jsonObject['device']['type'],
+        'timestamp': timestamp,
+        'location': {
+            'country': jsonObject['country'],
+            'city': jsonObject['city'],
+            'area': jsonObject['area'],
+            'factory': jsonObject['factory'],
+            'section': jsonObject['section']
+        },
+        'data': jsonObject['data']
+    }
+
+    return result
 
 
 def main (jsonObject):
